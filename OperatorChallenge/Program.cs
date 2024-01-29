a0bf8c81eca16c2b4fc88b4c7c5c2733060efa45
SarahIrons <sarahirons11@gmail.com>
2024-01-29 08:49:19 -0500

    Add project files.

---
+++ OperatorChallenge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OperatorChallenge
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            //In Visual Studio, utilize the != operator and display output to the console.
+            if (5
+!= 4)
+            {
+                Console.WriteLine(value: "The number on the left of the statement is not equal to number on the right.");
+            }
+            else
+            {
+                Console.WriteLine(value: "The numbers are not equal.");
+            }
+
+
+
+        }
+    }
+}
+
+
+
+
